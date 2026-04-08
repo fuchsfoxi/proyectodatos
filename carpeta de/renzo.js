849fc9689bea381abcdac2d8136322d9f2551641
fuchsfoxi <165415707+fuchsfoxi@users.noreply.github.com>
2026-04-07 21:17:22 -0500

    rfa

---
+++ carpeta de/renzo.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("menu-btn");
+    const menu = document.getElementById("menu");
+    const cerrar = document.getElementById("cerrar-menu");
+
+    btn.addEventListener("click", () => {
+        menu.style.display = "block";
+    });
+
+    cerrar.addEventListener("click", () => {
+        menu.style.display = "none";
+    });
+});
